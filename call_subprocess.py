f9b3670732d6b211e69873b098dd6f0f3de2f0cb
Matt McKellar-Spence <matt@mattms.com>
2015-01-30 23:20:46 +1100

    Add call subprocess and print output

--- call_subprocess.py
+++ call_subprocess.py
@@ -10,11 +10,12 @@ Links
 - https://docs.python.org/3/library/subprocess.html
 """
 
-from subprocess import call
+from subprocess import CalledProcessError, call, check_output
+import sys
 
 
 def call_and_check_errors():
-	call_args = ('wc', '-l', 'my_file')
+	call_args = ('touch', sys.argv[0])
 
 	call_args_str = ' '.join(call_args)
 	print(call_args_str)
@@ -25,5 +26,18 @@ def call_and_check_errors():
 		print('Error during call.')
 
 
+def call_and_get_output():
+	call_args = ('wc', '-l', sys.argv[0])
+
+	call_args_str = ' '.join(call_args)
+	print(call_args_str)
+
+	try:
+		stdout = check_output(call_args)
+		print(stdout)
+	except CalledProcessError:
+		print('Error during call.')
+
+
 if '__main__' == __name__:
-	call_and_check_errors()
+	call_and_get_output()
